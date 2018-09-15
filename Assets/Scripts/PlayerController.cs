52ac2b7c859c02f9cf7b07a8cded33b51640e857
Joshua Clark <jjclark@student.unimelb.edu.au>
2018-09-15 15:25:30 +1000

    snake follows mouse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,36 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
+    public float speed = 0.1f;
+
+    private Vector3 mousePosition;
+    //public float moveSpeed = 0.1f;
+
+    public int length;
+    public Text lengthText;
 
     private Rigidbody2D rb;
+    public List<Transform> tail = new List<Transform>();
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        /*foreach (Transform child in gameObject.transform)
+        {
+            if (child.tag == "Tail")
+            {
+                tail.Add(child);
+            }
+        }*/
     }
 
     private void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        //Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
-        rb.AddForce(movement * speed);
+
+            /*if (Input.GetMouseButton(1))
+            {*/
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = Vector2.MoveTowards(transform.position, mousePosition, speed);
+            //}
     }
 
-    /*void OnTriggerEnter2D(Collider2D other)
+
+        //rb.AddForce(movement * speed);
+
+        // MoveTail();
+    //}
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("PickUp"))
+        if (other.gameObject.CompareTag("Food"))
         {
             other.gameObject.SetActive(false);
-            count++;
-            SetCountText();
+            GrowTail();
+            //length++;
+            //lengthText.text = "Length: " + length.ToString();
+        }
+    }
+
+
+
+    void GrowTail() {
+        //tail.Add(new GameObject("tail"));
+        length++;
+        lengthText.text = "Length: " + length.ToString();
+    }
+
+    /*
+    void MoveTail() {
+        Follow(this.gameObject, tail[0]);
+        for (int i = 1; i < length-1; i++) {
+            Follow(tail[i], tail[i + 1]);
         }
     }*/
 
+
+    /*void Follow(GameObject leader, GameObject follower) {
+        // The step size is equal to speed times frame time.
+        float step = speed * Time.deltaTime;
+
+        // Move follower a step closer to the target.
+        follower.transform.position =
+            Vector3.MoveTowards(follower.transform.position, leader.transform.position, step);
+    }*/
 }
---
+++ Assets/Scripts/TailController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TailController : MonoBehaviour {
+
+    public float speed = 0.1f;
+    //private GameObject parent;
+
+    private Transform head;
+    private int tailNumber;
+
+
+
+    // Use this for initialization
+    void Start () {
+        head = GameObject.FindGameObjectWithTag("Player").gameObject.transform;
+        // better way to express this?
+        // use, find or findIndex...
+        for (int i = 0; i < head.GetComponent<PlayerController>().tail.Count; i++) {
+            if(gameObject == head.GetComponent<PlayerController>().tail[i].gameObject) {
+                tailNumber = i;
+            }
+        }
+
+        // parent = gameObject.transform.parent.gameObject;
+	}
+
+    private Vector2 movementSpeed;
+    public float timeStep = 0.5f;
+	void FixedUpdate () {
+        //Follow(parent, gameObject);
+
+        // if it is the first tail object, follow the head.
+        // otherwise follow the tail object before it in the tail list
+        Transform target = head;
+        if (tailNumber > 0)
+        {
+            target = head.GetComponent<PlayerController>().tail[tailNumber - 1];
+        }
+
+        transform.position = Vector2.SmoothDamp(transform.position, 
+                                                target.position, 
+                                                ref movementSpeed, 
+                                                timeStep);
+        // transform.LookAt(target.position);
+    }
+
+    /*
+
+    void MoveTail()
+    {
+        Follow(gameObject, tail[0]);
+        for (int i = 1; i < length - 1; i++)
+        {
+            Follow(tail[i], tail[i + 1]);
+        }
+    }
+
+
+    void Follow(GameObject leader, GameObject follower)
+    {
+        // The step size is equal to speed times frame time.
+        float step = speed * Time.deltaTime;
+
+        // Move follower a step closer to the target.
+        follower.transform.position =
+            Vector3.MoveTowards(follower.transform.position, leader.transform.position, step);
+    }
+    */
+
+
+}
