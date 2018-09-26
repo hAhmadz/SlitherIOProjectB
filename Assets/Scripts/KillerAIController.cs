383b09163193780e698c8e13584aa334089f3c6d
Joshua Clark <jjclark@student.unimelb.edu.au>
2018-09-26 16:07:04 +1000

    "killer" snake ai - kamikaze snake

--- Assets/Scripts/KillerAIController.cs
+++ Assets/Scripts/KillerAIController.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KillerAIController : MonoBehaviour {
+public class KillerAIController : AIController
+{
+    Transform targetHead;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override Vector2 FindTarget()
+    {
+        Vector2 target = GetCurrentTarget();
+        if (target.Equals(Vector2.zero) || transform.position.Equals(target))
+        {
+            var chance = Random.value;
+            print("chance : " + chance.ToString());
+            if (chance < 0.33f) // hunt player
+            {
+                targetHead = GameObject.FindGameObjectWithTag("Player").transform;
+                if (targetHead.Equals(null)) { return FindTarget(); }
+            }
+            else // hunt ai
+            {
+                GameObject[] AIHeads = GameObject.FindGameObjectsWithTag("AI");
+                foreach (GameObject head in AIHeads)
+                {
+                    if (!(head.transform.Equals(transform)))
+                    {
+                        targetHead = head.transform;
+                        break;
+                    }
+                }
+                if (targetHead.Equals(null)) { return FindTarget(); }
+            }
+
+        }
+        Vector2 myPos = transform.position;
+        Vector2 diff = myPos - target;
+
+        if (Mathf.Abs(diff.x) < 1 && Mathf.Abs(diff.x) < 1)
+            return diff * -1;
+        return targetHead.position;
+    }
 }
+       
