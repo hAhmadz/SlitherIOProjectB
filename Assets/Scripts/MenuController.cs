3381d64c016cea284318f691a1fdbe5bc4593c3d
Haaris <haaris.ahmad1@gmail.com>
2018-10-20 22:02:43 +1100

    user Class

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,7 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+    public string urlAddress;
+    public InputField FName;
+    public InputField LName;
+    public InputField userName;
+    public InputField pswd;
+
     //both play the same game at the moment from two different functions
     public void singlePlayerBtn()
     {
@@ -11,5 +21,30 @@ public class MenuController : MonoBehaviour
     public void multiPlayerBtn()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Scenes/MultiPlayerScene");
+    }
+
+    public void signUp()
+    {
+
+    }
+
+    IEnumerator Post(string url, string bodyJsonString)
+    {
+        string outputMsg = "";
+
+        var request = new UnityWebRequest(url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.Send();
+
+        Debug.Log("Status Code: " + request.responseCode);
+        if (request.responseCode.Equals(201))
+            outputMsg = "User Added";
+        else
+            outputMsg = "Cannot Add";
+
     }
 }
---
+++ Assets/Scripts/user.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class user
+    {
+        public string FirstName;
+        public string LastName;
+        public string UserName;
+        public string Password;
+
+        public user()
+        {
+            FirstName = "";
+            LastName = "";
+            UserName = "";
+            Password = "";
+        }
+        public user(string fName, string lName, string uName, string pswdd)
+        {
+            FirstName = fName;
+            LastName = lName;
+            UserName = uName;
+            Password = pswdd;
+        }
+    }
+}
