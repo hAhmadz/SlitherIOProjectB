eed77ea801af84d26aae055b22578f067c460176
Miner751 <drminer751@hotmail.com>
2018-10-21 23:06:32 +1100

    Partially re-implemented network

---
+++ Assets/Scripts/NetCameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetCameraController : MonoBehaviour {
+
+    public Transform player;
+
+    private Vector3 offset;
+
+
+	void Start () 
+    {
+        offset = transform.position - player.transform.position;
+	}
+	
+	void LateUpdate () 
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+
+    public void setTarget(Transform target)
+    {
+        player = target;
+        //Debug.Log(player == null);
+        offset = transform.position - player.transform.position;
+    }
+}
